e1fc0ac18b78328e3d146a9e614fa9ef8a690944
Kseniia Pikhotnik <pichotnik@gmail.com>
2020-10-13 20:50:40 +0300

    feat: create  new func

--- script/script.js
+++ script/script.js
@@ -1,29 +1,15 @@
 'use strict';
 
-const lang = prompt('Введите значение lang');
-
-if ( lang === 'ru') {
-console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-} else if ( lang === 'en') {
-console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+function getStr (str) {
+if (typeof str !== 'string') {
+  alert('Это не строка'); 
+}
+str = str.trim();
+if (str.length > 30) {
+  str = str.substr(0, 30) + '...';
+}
+  return str;
 }
 
-switch (lang) {
-case 'ru': 
-console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-break;
-case 'en':
-console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-}
-
-let dayArr = [];
-dayArr['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-dayArr['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-console.log(dayArr[lang]);
-
-const namePerson = prompt ('Введите имя');
-const message = (namePerson === 'Артем') ? 'директор' :
-(namePerson === 'Максим') ? 'преподаватель' :
-'студент';
-console.log(message);
+getStr (    '1111111111111111111111111111111'   );
 
